cdaa130c2efb8e14c39012d5e95d2f49969c0f57
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-04-05 13:44:22 +0700

    Prosedur input

--- Linear Search.cpp
+++ Linear Search.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n;
-int i;
+int arr[20]; // array to be searched
+int n;       // number of element in the array
+int i;       // indek of array element
+
+void input()
+{   
+    while (true)
+    {
+        cout << "Enter the number of element in the array : ";
+     cin >> n;
+     if ((n > 0) && (n <= 20))
+        break;
+     else
+        cout << "\nArray should have minimum 1 and maximum 20 element. \n\n";
+    
+    }
+
+    // Accept array elements
+    cout << "\n----------------------\n";
+    cout << "Enter array elements \n ";
+    cout << "------------------------\n";
+    for (i =0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+
+    
+    
+
+}
